a16412e5add74d35f1c262f9996ef7ddf4b5a205
vikas-bhat-d <139564925+vikas-bhat-d@users.noreply.github.com>
2023-07-15 16:33:46 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,11 +1,7 @@
 import random
 from words import word_list
-randword=random.choice(word_list).upper()
-#print(lrandword)
-#c=1
 
-
-def frequency(randword): #To find the frequnecy of a perticular letter in a randomly chosen word
+def frequency(randword):
     dic_freq={}
     for i in randword:
         if i in dic_freq:
@@ -88,18 +84,18 @@ def stages(tries):
 
 
 
-def game(randword): #main game logic is written here
+def game(randword):
     global a
     global b
     tries=6
     freq={}
     result="loose"
     guessed=False
-    guessed_letters=[] #To store already guessed letters
+    guessed_letters=[]
     lrandword=list(randword)
     word_completed='-'*len(lrandword)
     #print(word_completed)
-    compword=list(word_completed)#this variable holds the progress we made guessing the word as list
+    compword=list(word_completed)
     a=random.randint(0,len(lrandword)-1)
     try_=False
     while (try_==False):
@@ -108,9 +104,9 @@ def game(randword): #main game logic is written here
             try_=False
         else:
             try_=True
-    compword[a]=lrandword[a] #it will randomly assign a letter into compword as a hint
-    compword[b]=lrandword[b] #it will randomly assign another letter into compword as a hint
-    compword_="".join(compword)#this joins to list elements into single string
+    compword[a]=lrandword[a]
+    compword[b]=lrandword[b]
+    compword_=" ".join(compword)
     guessed_letters.append(lrandword[a])
     guessed_letters.append(lrandword[b])
     print("\n\n",compword_,"\n\n") #display mechanism
@@ -118,8 +114,8 @@ def game(randword): #main game logic is written here
     
 
 
-    
-    while guessed==False and (tries>0): 
+ 
+    while guessed==False and (tries>0):
         ans=input("Please guess letter or word:").upper()
         if(len(ans)==1 and ans.isalpha()):
             if(ans in guessed_letters):
@@ -131,12 +127,17 @@ def game(randword): #main game logic is written here
                 
                     for i in range(0,len(lrandword)):
                         if(str(lrandword[i])==str(ans)):
-                            
+                            #if (lrandword[i]!=lrandword[a] and lrandword[i]!=lrandword[b]):
+                                 #global value
+                                 #value=i
+                                 #compword[value]=ans 
+                                 
+                            #else:
                             value=i
                             compword[value]=ans 
                             guessed_letters.append(ans)
                             
-                            compword_="".join(compword)
+                            compword_=" ".join(compword)
                             print(compword_)
                             stages(tries)
                             if(compword==lrandword):
@@ -167,7 +168,7 @@ def game(randword): #main game logic is written here
                         
                         value=i
                         compword[value]=ans
-                        compword_="".join(compword)
+                        compword_=" ".join(compword)
                         print(compword_)  
                         stages(tries)
                         
@@ -177,7 +178,7 @@ def game(randword): #main game logic is written here
                                 result="win"
                                
                     
-        
+        #elif(len(ans)==len(randword)):
 
         else:
             print("invalid entry")
@@ -185,19 +186,9 @@ def game(randword): #main game logic is written here
     response=str(input("Do you wish to play again? (y/n) ").upper()) 
     return response
 def main():
+    
+    randword=random.choice(word_list).upper()
+    #print(lrandword)
+    #c=1
+
     game(randword)
-    
-    
-    while(response=="Y"):
-        game(randword)
-
-main()
-
-
-
-
-
-
-
-        
-
